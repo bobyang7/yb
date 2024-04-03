a3cd004f5e1f85fc8967f82bcb199140c1f8be3e
bby <bobyang677@gmail.com>
2024-04-03 19:27:51 +0000

    del

--- a.py
+++ a.py
@@ -2,11 +2,3 @@ import torch
 
 a = torch.tensor([1, 2, 3])
 b = torch.tensor([4, 5, 6])
-c = torch.tensor([1, 2, 3])
-d = torch.tensor([5, 6, 7])
-e = torch.tensor([4, 5, 6])
-f = torch.tensor([1, 2, 3])
-g = torch.tensor([1, 2, 3])
-h = torch.tensor([4, 5, 6])
-x = torch.tensor([1, 2, 3])
-y = torch.tensor([1, 2, 3])
